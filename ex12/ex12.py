86f7e00f0cf2678a1c993cb70b1ee8fe38ea2db8
patryk.silkowski <patryk.silkowski@sigma.se>
2019-10-23 10:13:53 +0200

    some exercises

---
+++ ex12/ex12.py
@@ -0,0 +1,5 @@
+age = input("How old are you? ")
+heigth = input("How tall are you? ")
+weight = input("How much do you weight? ")
+
+print(f"So, you're {age} old, {heigth} tall and {weight} heavy.")
---
+++ ex2/ex2.py
@@ -0,0 +1,5 @@
+# typical comment
+print("I could have code like this.") # and the comment after is ignored
+
+# print("This won't run.")
+print("This will run.")
